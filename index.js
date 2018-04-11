7d7126f8d1929107f4e2a32e6e678608442e4685
Edgar Spickerman <spickerman.edgar@gmail.com>
2018-04-11 00:43:40 -0700

    added some more functionality

--- index.js
+++ index.js
@@ -1,45 +1,33 @@
-const Word = require("./lib/constructors/Word");
-const CLI = require("./lib/constructors/Cli");
-const wordBank = require("./lib/data/wordBank");
+const Game = require("./lib/constructors/Game");
+const ScoreBoard = require("./lib/constructors/ScoreBoard");
+const cli = require("./lib/display/cli");
+const resultMsg = require("./lib/data/messages").resultMsg;
+const words = require("./lib/data/wordBank").words
 
-const Game = (words) => {
-  const addWin = () => {};
-  const addLoss = () => {};
-  const generateWord = (words) => {
-    const genRandomIndex = (words) => Math.floor(Math.random() * words.length);
-    return words[genRandomIndex(words)];
-  };
+const scoreboard = ScoreBoard();
 
-  return {
-    count:6,
-    word: Word(generateWord(words)),
-    cli: CLI(),
-    validations:function(){
-      if(this.word.hangmanWord().indexOf("_") === -1) return true;
-      if(this.count === 0) return true;
-    },
-    start: function () {
-      return this.cli.welcomeMessage().then(data=>'');
-    },
-    takeTurns:function(){
-      if(this.validations()) return this.continue()
-      return this.nextTurn().then(word=>this.cli.print(word))
-        .then(this.takeTurns())
-    },
-    nextTurn: function () {
-      return this.cli.prompt().then(answer=>{
-        this.word.guess(answer.choice_question);
-        return this.word.hangmanWord()
-      })
-    },
-    continue:function(){
+const startGame = (words, results, display) => {
+  let game = Game();
 
-    },
+  const startRound=()=>{
+    return cli.prompt()
+      .then(answer => game.playRound(answer.choice))
+      .then(result => cli.print(result))
+      .then(() => playRounds())
   }
+
+  const playRounds = () => {
+    if (game.isHanged()) return resultMsg.winning;
+    if (game.getGuesses() === 6) return resultMsg.lossing;
+    return startRound()
+  }
+
+  game.loading(words);
+
+  cli.welcomeMessage()
+    .then(() => playRounds())
+    .then(msg => cli.print(msg))
+    .catch(err => console.log(err));
 }
 
-let game = Game(wordBank);
-
-game.start()
-  .then(data=> game.takeTurns())
-  .catch(error => console.log("An Error occurred:", error))
+startGame(words, scoreboard, cli)
